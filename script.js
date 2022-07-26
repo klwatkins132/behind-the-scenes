18e99f529a1ce9a609a3f8e3547e8ab0741af5f5
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-26 17:03:54 -0600

    scoping pt2

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
 'use strict';
 ///////////////////////////////////////
-// Scoping pt1
+// Scoping pt2
 
+function calcAge(birthYear) {
+    const age = 2022 - birthYear;
+
+    //print age scope
+    function printAge() {
+        let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+        console.log(output);  // prints 'Kelsy, You are 31, born in 1991'
+
+        // if block scope
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+
+            //creating NEW variable with same name as outer scopes variable
+            const firstName = 'Steven';  // this will print 'Steven' becuase javascript looks upwards in the scope and will see this first
+
+            // Reassagning outer scopes variable
+            output = 'NEW OUTPUT'; // this will print 'NEW OUTPUT' because child scope redefines from outser parent scope            
+
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);  // prints 'Oh, and you're a millenial, Steven'
+
+            function add(a, b) {
+                return a + b;
+            }
+
+        }
+        // console.log(add(2, 3)); // *Reference error*
+        // console.log(str); // *Reference error*
+        console.log(millenial); // prints true
+        console.log(output); // prints'NEW OUTPUT'
+    }
+    printAge();
+    return age;
+}
+const firstName = 'Kelsy'; // global variable 
+calcAge(1991);
+
+
+
+///////////////////////////////////////
+// Scoping pt1
+/*
 function calcAge(birthYear) {  // global scope
     const age = 2022 - birthYear;
 
@@ -29,3 +71,4 @@ calcAge(1991);
 //console.log(age); // *Reference error* you don't have access to 'age'
 //printAge(); //*Reference error* only allowed in calcAge scope
 //console.log(millenial); // *Reference error* var is function scoped
+*/
